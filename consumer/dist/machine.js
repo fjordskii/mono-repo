c2b686292121fcc836f9855a6138661c1a14c4c9
Ford Heacock <ford.heacock@dapperlabs.com>
2021-12-20 11:07:15 -0500

    build simple machine

--- consumer/dist/machine.js
+++ consumer/dist/machine.js
@@ -1,24 +1,21 @@
 import { createMachine, Interpreter } from 'xstate';
-
 // Stateless machine definition
 // machine.transition(...) is a pure function used by the interpreter.
 const toggleMachine = createMachine({
-  id: 'toggle',
-  initial: 'inactive',
-  states: {
-    inactive: { on: { TOGGLE: 'loading' } },
-    loading: { on: { TOGGLE: 'inactive' } },
-  },
+    id: 'toggle',
+    initial: 'inactive',
+    states: {
+        inactive: { on: { TOGGLE: 'loading' } },
+        loading: { on: { TOGGLE: 'inactive' } },
+    },
 });
-
 // Machine instance with internal state
 const toggleService = new Interpreter(toggleMachine)
-  .onTransition((state) => console.log(state.value))
-  .start();
+    .onTransition((state) => console.log(state.value))
+    .start();
 // => 'inactive'
-
 toggleService.send('TOGGLE');
 // => 'loading'
-
 toggleService.send('TOGGLE');
 // => 'inactive'
+//# sourceMappingURL=machine.js.map
